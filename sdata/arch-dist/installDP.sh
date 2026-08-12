f768390c7226bffc2c9cd5ce6ede3b59ccc64bc1
Solanaceae <89823371+0xSolanaceae@users.noreply.github.com>
2026-08-11 20:48:48 -0400

    chore: prep for nmcli migration to dbus calls

--- sdata/arch-dist/installDP.sh
+++ sdata/arch-dist/installDP.sh
@@ -32,6 +32,7 @@ CORE_PACKAGES=(
     cmake ninja ccache
     wl-clipboard cliphist wl-clip-persist inotify-tools app2unit wireplumber trash-cli jq aubio lm_sensors
     libpipewire glibc libcava qt6-declarative gcc-libs qt6-base qt6-declarative qt6-wayland libqalculate kpipewire kglobalaccel kglobalacceld libsecret
+    networkmanager-qt
     ffmpeg
 )
 
